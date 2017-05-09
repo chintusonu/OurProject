538b6c99e36723921b3f2968561ceb66a72a61d4
chintu <daschintu1996@gmail.com>
2017-05-09 14:59:47 +0530

    complete task

--- CoreBanking/src/main/java/com/corebanking/dao/NewAccountRegDAO.java
+++ CoreBanking/src/main/java/com/corebanking/dao/NewAccountRegDAO.java
@@ -3,6 +3,6 @@ package com.corebanking.dao;
 import com.corebanking.domain.NewAccountRegBO;
 
 public interface NewAccountRegDAO {
-	
+	//save CustomerDetails
 	public int saveNewCustomerData(NewAccountRegBO bo);
 }
