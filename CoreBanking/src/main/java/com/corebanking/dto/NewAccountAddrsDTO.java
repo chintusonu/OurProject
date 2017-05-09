bd2ebbe103e8fc15e482decd09a0015f889aad2c
Trilochan5 <trilochanpandait@gmail.com>
2017-05-09 15:42:20 +0530

    sonu-addrs dto

---
+++ CoreBanking/src/main/java/com/corebanking/dto/NewAccountAddrsDTO.java
@@ -0,0 +1,50 @@
+package com.corebanking.dto;
+
+import java.io.Serializable;
+
+public class NewAccountAddrsDTO implements Serializable{
+	private String ADDRESS_TYPE;
+	private String ADDRESS;
+	private String STATE;
+	private Integer PIN;
+	private String CITY;
+	private Long MOBILE_NO;
+	public String getADDRESS_TYPE() {
+		return ADDRESS_TYPE;
+	}
+	public void setADDRESS_TYPE(String aDDRESS_TYPE) {
+		ADDRESS_TYPE = aDDRESS_TYPE;
+	}
+	public String getADDRESS() {
+		return ADDRESS;
+	}
+	public void setADDRESS(String aDDRESS) {
+		ADDRESS = aDDRESS;
+	}
+	public String getSTATE() {
+		return STATE;
+	}
+	public void setSTATE(String sTATE) {
+		STATE = sTATE;
+	}
+	public Integer getPIN() {
+		return PIN;
+	}
+	public void setPIN(Integer pIN) {
+		PIN = pIN;
+	}
+	public String getCITY() {
+		return CITY;
+	}
+	public void setCITY(String cITY) {
+		CITY = cITY;
+	}
+	public Long getMOBILE_NO() {
+		return MOBILE_NO;
+	}
+	public void setMOBILE_NO(Long mOBILE_NO) {
+		MOBILE_NO = mOBILE_NO;
+	}
+	
+	
+}
