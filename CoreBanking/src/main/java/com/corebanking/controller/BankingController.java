72e95c3a4534c04972b64c26c0d65212d7e1ee0d
Trilochan5 <trilochanpandait@gmail.com>
2017-05-09 16:16:53 +0530

    sonu

--- CoreBanking/src/main/java/com/corebanking/controller/BankingController.java
+++ CoreBanking/src/main/java/com/corebanking/controller/BankingController.java
@@ -1,8 +1,15 @@
 package com.corebanking.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BankingController {
+	
+	@RequestMapping(value="getRegForm.htm")
+	public String getNewRegistrationForm(){
+		return "newAccRegForm";
+	}
+	
 	
 }
--- CoreBanking/src/main/java/com/corebanking/service/NewAccountRegServiceImpl.java
+++ CoreBanking/src/main/java/com/corebanking/service/NewAccountRegServiceImpl.java
@@ -1,7 +1,10 @@
 package com.corebanking.service;
 
+import org.springframework.stereotype.Service;
+
 import com.corebanking.dto.NewAccountRegDTO;
 
+@Service
 public class NewAccountRegServiceImpl implements NewAccountRegService {
 
 	@Override
