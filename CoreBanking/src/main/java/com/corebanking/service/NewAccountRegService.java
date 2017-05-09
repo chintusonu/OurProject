1508c347d25313d2f6ae7dd23ce95bab05ddc52f
Trilochan5 <trilochanpandait@gmail.com>
2017-05-09 15:44:28 +0530

    sonu-service

---
+++ CoreBanking/src/main/java/com/corebanking/service/NewAccountRegService.java
@@ -0,0 +1,7 @@
+package com.corebanking.service;
+
+import com.corebanking.dto.NewAccountRegDTO;
+
+public interface NewAccountRegService {
+	public String registerAccount(NewAccountRegDTO dto);
+}
---
+++ CoreBanking/src/main/java/com/corebanking/service/NewAccountRegServiceImpl.java
@@ -0,0 +1,13 @@
+package com.corebanking.service;
+
+import com.corebanking.dto.NewAccountRegDTO;
+
+public class NewAccountRegServiceImpl implements NewAccountRegService {
+
+	@Override
+	public String registerAccount(NewAccountRegDTO dto) {
+		
+		return null;
+	}
+
+}
