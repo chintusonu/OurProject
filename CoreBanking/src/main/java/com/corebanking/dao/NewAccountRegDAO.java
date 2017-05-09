9676b4f46b922bede0a43172e646a5ce759af807
chintu <daschintu1996@gmail.com>
2017-05-09 14:43:38 +0530

    hii

--- CoreBanking/src/main/java/com/corebanking/dao/NewAccountRegDAO.java
+++ CoreBanking/src/main/java/com/corebanking/dao/NewAccountRegDAO.java
@@ -2,7 +2,7 @@ package com.corebanking.dao;
 
 public interface NewAccountRegDAO {
 	
-	public void saveNewCustomerData();
+	public int saveNewCustomerData();
 	
 
 }
--- CoreBanking/src/main/java/com/corebanking/dao/NewAccountRegDAOImpl.java
+++ CoreBanking/src/main/java/com/corebanking/dao/NewAccountRegDAOImpl.java
@@ -1,18 +1,11 @@
 package com.corebanking.dao;
 
-import org.hibernate.Session;
-
-import com.corebanking.utility.HibernateUtil;
 
 public class NewAccountRegDAOImpl implements NewAccountRegDAO {
 
 	@Override
-	public void saveNewCustomerData() {
-		Session session=null;
-		//get session
-		session = HibernateUtil.getSession();
-		//prepare objects
-		
+	public int saveNewCustomerData() {
+
 		
 	}
 
